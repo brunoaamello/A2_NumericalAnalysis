239fc2d4bdbfb3464635d355c15cb56ad21ff424
brunoaamello <brunoaamello@gmail.com>
2018-12-10 21:44:03 -0200

    Added Gauss-Legendre integration, finished code with comments.

--- t2calcnum.c
+++ t2calcnum.c
@@ -1,51 +1,74 @@
 /* Assignment 2 - Numerical Analysis
  * 2nd semester of 2018
  * by: Bruno Arantes
+ * Avaiable in: https://github.com/brunoaamello/A2_NumericalAnalysis
  */
 /* Trabalho 2 - Calculo Numerico
  * 2o semestre de 2018
  * USP Sao Carlos
  * Bruno Arantes de Achilles Mello - 9866490
+ * Disponivel em: https://github.com/brunoaamello/A2_NumericalAnalysis
  */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 
+//Erro para cálculo de N nos métodos de quadratura compostos
 #define MAX_ERROR 1.0e-8
-#define SIG_DIGITS 10
 
+//Digitos para serem impressos
+#define SIG_DIGITS 12
 
+//Declaração das estruturas
 typedef struct {
 	double first;
 	double second;
 } pair_double;
-
 typedef struct {
 	double (*f)(double);
 	double max2;
 	double max4;
 } Function;
 
+//Função para impressão de pontos flutuantes com vários dígitos e com forma que permita fácil visualização
 void printDouble(double number){
 	printf("%+.*e ", SIG_DIGITS, number);
 }
 
+//Função desejada
 double myFunc(double x){
 	return exp(-0.5*x*x);
 }
 
+//Função equivalente para integração de -1 a 1
+double myEqFunc(double x){
+	return 0.5*exp(-0.125*(x+1)*(x+1));
+}
+
+//Uso da fórmula do trapézio
+//Função para cáculo do número de subintervalos
 unsigned evaluateIntervals_tra(Function func, pair_double interval, double error);
+//Função para cálculo da integral com a fórmula do trapézio composta
 double integrate_tra(Function func, pair_double interval, unsigned subIntervals);
 
+//Uso da fórmula de simpson 1/3
+//Função para cálculo do número de subintervalos, sempre retorna um valor par
 unsigned evaluateIntervals_simp13(Function func, pair_double interval, double error);
+//Função para cálculo da integral com a fórmula de simpson 1/3 composta, assume subIntervals par
 double integrate_simp13(Function func, pair_double interval, unsigned subIntervals);
 
+//Função para cálculo da integral com a quadratura de Gauss-LEGENDRE, com n = 6, assume intervalo de -1 a 1
+double integrate_gauss(Function func, unsigned n);
+
 int main(){
-	Function func;
+	Function func, func2;
 	func.f = myFunc;
 	func.max2 = 1.0;
 	func.max4 = 3.0;
+	func2.f = myEqFunc;
+	func2.max2 = 1.0;
+	func2.max2 = 3.0;
 	pair_double interval;
 	interval.first = 0.0;
 	interval.second = 1.0;
@@ -68,6 +91,12 @@ int main(){
 	result = integrate_simp13(func, interval, n);
 	printf("Is(f) = ");
 	printDouble(result);
+	printf("\n\n");
+	
+	printf("Calculando a integral com o método de Gauss-LEGENDRE para n=6...\n");
+	result = integrate_gauss(func2, 6);
+	printf("Il(f) = ");
+	printDouble(result);
 	printf("\n");
 	
 	
@@ -87,7 +116,7 @@ double integrate_tra(Function func, pair_double interval, unsigned subIntervals)
 	increment = (interval.second-interval.first)/subIntervals;
 	x = interval.first + increment;
 	result = (func.f(interval.first)+func.f(interval.second))/2.0;
-	for(i=1;i<subIntervals;i++, x+=increment){
+	for(i=1;i<=subIntervals-1;i++, x+=increment){
 		result+=func.f(x);
 	}
 	result*=increment;
@@ -112,7 +141,7 @@ double integrate_simp13(Function func, pair_double interval, unsigned subInterva
 	increment = (interval.second-interval.first)/subIntervals;
 	x = interval.first + increment;
 	result = 0.0;
-	for(i=1;i<(subIntervals/2);i++, x+=increment){
+	for(i=1;i<=(subIntervals/2)-1;i++, x+=increment){
 		result+=2.0*func.f(x);
 		x+=increment;
 		result+=1*func.f(x);
@@ -121,4 +150,13 @@ double integrate_simp13(Function func, pair_double interval, unsigned subInterva
 	result+=(func.f(interval.first)+func.f(interval.second)+4.0*func.f(interval.second-increment));
 	result*=(increment/3.0);
 	return result;
+}
+
+double integrate_gauss(Function func, unsigned n){
+	switch(n){
+		case 6:
+			return (0.171324*(func.f(0.949108)+func.f(-0.949108)))+(0.360761*(func.f(0.661209)+func.f(-0.661209)))+(0.467959*(func.f(0.238619)+func.f(-0.238619)));
+		default:
+			return 0.0;
+	}
 }
